9d291234e00ede56c75c64ea6e037c87be6506f3
Cole Burrell <colebee@mail.fresnostate.edu>
2017-10-17 16:40:26 -0700

    refactor email check

--- js/check_email.js
+++ js/check_email.js
@@ -1,24 +1,38 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 userEmail = 'colebee@mail.fresnostate.edu';
+//userEmail = 'nonexist@nte.mail';
 keyServer = 'http://192.241.239.122:8888';
 
 var response;
 
+// Pass in the user's email, the keyserver address, and a callback function
+// that takes in a boolean
+//
+// for example:
+// reg_checkEmailAvailable(userEmail, keyServer, function(responseBool){
+//   console.log(responseBool)
+// });
 function reg_checkEmailAvailable(userEmail, keyServer, callback)
 {
-  var re;
   var retrieveAPI = '/api/v1/key?email=';
   var request = keyServer.concat(retrieveAPI, userEmail);
   var xhr = new XMLHttpRequest();
   xhr.open("GET", request, true);
   xhr.onload = function () {
-    var content = JSON.parse(xhr.responseText);
-    if (xhr.readyState == 4 && xhr.status == "200") {
-      console.table(content);
-      callback(!content.userIds[0].verified);
+    if (xhr.readyState == 4) {
+      if (xhr.responseText == "Key not found") {
+        callback(true);
+      } else {
+        var content = JSON.parse(xhr.responseText);
+        if (content.userIds[0].verified == true) {
+          callback(false);
+        } else {
+          callback(true);
+        }
+      } 
     } else {
-      console.table(content);
+      console.error(xhr.readyState);
     }
   }
   xhr.send(null);
