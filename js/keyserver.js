10177cf4c5fd66061de345e224fad50b9bacee79
arobe91594 <31674273+arobe91594@users.noreply.github.com>
2017-11-29 17:26:25 -0800

    Moved verify_email to module

--- js/keyserver.js
+++ js/keyserver.js
@@ -79,7 +79,7 @@ Keyserver.checkEmailAvailable = function (userEmail, keyServer, callback) {
   xhr.send(null);
 }
 
-function verify_email(message) {
+Keyserver.verify_email = function(message) {
   var decoded = message.replace(/=3D/g, '=').replace(/=\s*$/m, '');
   var beginUrl = decoded.slice(decoded.search('http'), -1);
   var match = beginUrl.match(/[^\r\n]+/g);
